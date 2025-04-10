92e6d9a14440b86132f22797786730bb23ecabf3
warley farias <157419864+warrley@users.noreply.github.com>
2025-04-10 06:37:53 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Screen() {
+    return (
+        <View className="text-red-500">
+            <Text className="text-red-500">cuida</Text>
+        </View>
+    )
+}
